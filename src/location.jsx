ac8802dc5e13fd57cf03f647a7528441a16daf2d
Abdulaziz-developer1 <tohirovabdulaziz11@gmail.com>
2025-07-28 11:29:48 +0500

    change

--- src/location.jsx
+++ src/location.jsx
@@ -12,7 +12,8 @@ function location({lang}) {
       </div>
       <br />
       <div className="flex left50">
-        <img src={map} alt="map img" />
+        {/* <img src={map} alt="map img" /> */}
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d532.2717851533571!2d71.67407519644242!3d41.00372537386015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bb4d002546dfed%3A0xb9bab87fb75370ba!2sAlgoritm%20IT%20Center!5e0!3m2!1sru!2s!4v1753683636110!5m2!1sru!2s" width="600" height="450" style={{border:0}} loading="lazy"></iframe>
         <div className="consultation">
           <h2>
             <span>{Lang[0].consultation.title[lang]}</span>
